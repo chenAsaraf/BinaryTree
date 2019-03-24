28a72abe36b3e8b87f69bed90dbdf215d2910a5b
orenIsabella <33618982+orenIsabella@users.noreply.github.com>
2019-03-24 21:12:01 +0200

    Update Tree.hpp

--- Tree.hpp
+++ Tree.hpp
@@ -16,22 +16,22 @@ class Node {
         Node(int data, Node* right, Node* left, Node* parent) :
         data(data), right(right), left(left), parent(parent) {}
          
-        int& data(); //setter
-        const int data() const; //getter
-        Node*& right();  //setter ->> how to write Node*&?
-        const Node* right const; //getter
-        Node*& left(); //setter
-        const Node* left() const; //getter
-        string toString() const;
+        // int& data(); //setter
+        // const int data() const; //getter
+        // Node*& right();  //setter ->> how to write Node*&?
+        // const Node* right const; //getter
+        // Node*& left(); //setter
+        // const Node* left() const; //getter
+        // string toString() const;
 };
 
 class Tree {
     private:
         Node* root;
-        
+        int size;
         /*Private Methods*/
-        void destroyTree(Node* node);
-        Node insert(Node* node, int key);
+        void destroyTree();
+        Node insert(int i);
         
     public:
         //constructor & deconstructor
@@ -41,21 +41,21 @@ class Tree {
         /*Public Methods*/
         void destroyTree();
         //insert the number i to the correct node in the binary tree
-        Node insert(int i); //throw exception when already axist
+        void insert(int i); //throw exception when already axist
         //remove the number i from the tree
         void remove(int i); //throw exception when not exist
         //returns the size of the tree
-        int size() const;
+        int size();
         //returns "true" if the number i exists in the tree
         bool contains(int i);
         //returns the data in the root
         int root();
         //returns the parent's data of the input
-        int parent(int child);
+        int parent(int i);
         //returns the left child's data of the input
-        int left(int parent);
+        int left(int i);
         //returns the right child's data of the input
-        int right(int parent);
+        int right(int i);
         //print the tree
         void print();
         
