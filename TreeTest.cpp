dd878ab62ff3bc5b4303ee3585402e8811d63ad4
danivngopro <44786079+danivngopro@users.noreply.github.com>
2019-03-26 09:46:17 +0000

    Update TreeTest.cpp

--- TreeTest.cpp
+++ TreeTest.cpp
@@ -13,9 +13,24 @@ using std::cout, std::endl;
 int main() {
   ariel::Tree emptytree;
   ariel::Tree threetree;  
+  ariel::Tree smalltree;
+  ariel::Tree bigtree;  
+
   threetree.insert(5);
   threetree.insert(7);
   threetree.insert(3);
+
+  smalltree.insert(16);
+  smalltree.insert(2);
+
+  bigtree.insert(520);
+  bigtree.insert(52);
+  bigtree.insert(64);
+  bigtree.insert(25);
+  bigtree.insert(4500);
+  bigtree.insert(7800);
+  bigtree.insert(6000);
+
   
   ariel::Tree mytree;  
 
@@ -39,6 +54,35 @@ int main() {
   .CHECK_THROWS(threetree.insert(3))
   .CHECK_THROWS(threetree.left(6))
   .CHECK_OK    (threetree.print())
+
+  .CHECK_EQUAL (smalltree.size(),2)
+  .CHECK_THROWS(smalltree.remove(2341))
+  .CHECK_OK    (smalltree.remove(16))
+  .CHECK_EQUAL (smalltree.size(),1)
+  .CHECK_EQUAL (smalltree.root(),2)
+  .CHECK_THROWS(smalltree.parent(2))
+  .CHECK_OK    (smalltree.insert(1))
+  .CHECK_OK    (smalltree.insert(3))
+  .CHECK_EQUAL (smalltree.left(2),1)
+  .CHECK_EQUAL (smalltree.right(2),3)
+  .CHECK_OK    (smalltree.print())
+
+  .CHECK_OK    (bigtree.remove(7800))
+  .CHECK_EQUAL (bigtree.right(4500),6000)
+  .CHECK_OK    (bigtree.remove(52))
+  .CHECK_EQUAL (bigtree.left(520),25)
+  .CHECK_EQUAL (bigtree.right(25),64)
+  .CHECK_OK    (bigtree.remove(520))
+  .CHECK_EQUAL (bigtree.parent(4500),64)
+  .CHECK_THROWS(bigtree.parent(64))
+  .CHECK_OK    (bigtree.remove(64))
+  .CHECK_OK    (bigtree.insert(64))
+  .CHECK_EQUAL (bigtree.parent(64),4500)
+  .CHECK_EQUAL (bigtree.parent(4500),25)
+  .CHECK_EQUAL (bigtree.size(),4)
+  .CHECK_EQUAL (bigtree.right(4500),6000)
+  .CHECK_OK    (bigtree.print())
+
   .print();
   
   cout << "You have " << tc.right() << " right answers and " << tc.wrong() << " wrong answers so your grade is " << tc.grade() << ". Great!" << endl;
